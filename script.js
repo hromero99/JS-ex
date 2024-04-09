cde596244cd40672248f0ce635617bd3a88699a0
Héctor Romero López <hector@hromerol.es>
2024-04-09 17:06:34 +0200

    obj cards

--- script.js
+++ script.js
@@ -1,255 +1,620 @@
-const data = [
+const cards = [
 	{
-		"id": 1,
-		"firstname": "Hector",
-		"lastname": "Julien",
-		"email": "vallet.eugene@noos.fr",
-		"phone": "+5109160268056",
-		"birthday": "1981-11-13",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "99, rue Labbe",
-			"streetName": "place Imbert",
-			"buildingNumber": "26",
-			"city": "Leclercq-sur-Raynaud",
-			"zipcode": "77 231",
-			"country": "Ghana",
-			"county_code": "PN",
-			"latitude": -9.516893,
-			"longitude": 82.740856
-		},
-		"website": "http://humbert.com",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "MasterCard",
+		"number": "4532750907210784",
+		"expiration": "08/24",
+		"owner": "Max Raynor"
 	},
 	{
-		"id": 2,
-		"firstname": "Odette",
-		"lastname": "Bonneau",
-		"email": "martin.millet@ifrance.com",
-		"phone": "+5289858020588",
-		"birthday": "1946-09-24",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "833, impasse Alain Arnaud",
-			"streetName": "chemin Jacques Bonnin",
-			"buildingNumber": "6",
-			"city": "Bonnet",
-			"zipcode": "84 863",
-			"country": "Tunisie",
-			"county_code": "LS",
-			"latitude": 56.883101,
-			"longitude": -15.200751
-		},
-		"website": "http://herve.net",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Visa Retired",
+		"number": "2521245243761597",
+		"expiration": "05/25",
+		"owner": "Stella Gerhold"
 	},
 	{
-		"id": 3,
-		"firstname": "Franck",
-		"lastname": "Jean",
-		"email": "antoine.pruvost@camus.org",
-		"phone": "+9942448958977",
-		"birthday": "1956-08-02",
-		"gender": "male",
-		"address": {
-			"id": 0,
-			"street": "86, chemin Alexandre",
-			"streetName": "boulevard Fernandez",
-			"buildingNumber": "76",
-			"city": "Blanc",
-			"zipcode": "45730",
-			"country": "Bahrain",
-			"county_code": "MX",
-			"latitude": -33.092096,
-			"longitude": -17.53356
-		},
-		"website": "http://bonnin.org",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "American Express",
+		"number": "4024007178484134",
+		"expiration": "12/26",
+		"owner": "Lilliana Shanahan"
 	},
 	{
-		"id": 4,
-		"firstname": "Yves",
-		"lastname": "Guillaume",
-		"email": "klein.danielle@free.fr",
-		"phone": "+2441886750696",
-		"birthday": "1971-07-20",
-		"gender": "male",
-		"address": {
-			"id": 0,
-			"street": "72, impasse Franck Huet",
-			"streetName": "impasse Gillet",
-			"buildingNumber": "62",
-			"city": "Chauvin-sur-Leclercq",
-			"zipcode": "91 319",
-			"country": "Cor\u00e9e du Nord",
-			"county_code": "SS",
-			"latitude": 41.868855,
-			"longitude": -113.61381
-		},
-		"website": "http://marie.fr",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Visa",
+		"number": "5230345408485662",
+		"expiration": "09/24",
+		"owner": "Christina Gibson"
 	},
 	{
-		"id": 5,
-		"firstname": "Olivier",
-		"lastname": "Lefort",
-		"email": "andree36@club-internet.fr",
-		"phone": "+8767765303245",
-		"birthday": "1938-06-27",
-		"gender": "male",
-		"address": {
-			"id": 0,
-			"street": "66, rue Michelle Costa",
-			"streetName": "chemin de Lucas",
-			"buildingNumber": "23",
-			"city": "Gaudin-sur-Bodin",
-			"zipcode": "32296",
-			"country": "Madagascar",
-			"county_code": "IM",
-			"latitude": -74.989737,
-			"longitude": -122.345243
-		},
-		"website": "http://gaillard.com",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Visa",
+		"number": "2221179050665513",
+		"expiration": "11/26",
+		"owner": "Jake Feil"
 	},
 	{
-		"id": 6,
-		"firstname": "Val\u00e9rie",
-		"lastname": "Delmas",
-		"email": "penelope.robert@yahoo.fr",
-		"phone": "+3523805025464",
-		"birthday": "1941-08-04",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "93, rue Marie",
-			"streetName": "impasse Nath Chretien",
-			"buildingNumber": "47",
-			"city": "MauriceBourg",
-			"zipcode": "81 549",
-			"country": "Liechtenstein",
-			"county_code": "SE",
-			"latitude": -73.228572,
-			"longitude": 137.995544
-		},
-		"website": "http://lebon.org",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Discover Card",
+		"number": "4024007108573097",
+		"expiration": "10/26",
+		"owner": "Victor Bashirian"
 	},
 	{
-		"id": 7,
-		"firstname": "Vincent",
-		"lastname": "Dos Santos",
-		"email": "mary.michel@club-internet.fr",
-		"phone": "+1977700117415",
-		"birthday": "2023-09-22",
-		"gender": "male",
-		"address": {
-			"id": 0,
-			"street": "1, rue de Andre",
-			"streetName": "rue Humbert",
-			"buildingNumber": "26",
-			"city": "Guilletnec",
-			"zipcode": "89 989",
-			"country": "Marshall (\u00celes)",
-			"county_code": "PH",
-			"latitude": 25.117105,
-			"longitude": -71.700188
-		},
-		"website": "http://joubert.com",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "MasterCard",
+		"number": "4556101006271874",
+		"expiration": "05/25",
+		"owner": "Pearline Stroman"
 	},
 	{
-		"id": 8,
-		"firstname": "Antoinette",
-		"lastname": "Lelievre",
-		"email": "marianne.bertin@wanadoo.fr",
-		"phone": "+3921816799483",
-		"birthday": "2022-02-17",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "47, impasse Thomas Hebert",
-			"streetName": "impasse de Gaudin",
-			"buildingNumber": "633",
-			"city": "Roche",
-			"zipcode": "74565",
-			"country": "Sainte H\u00e9l\u00e8ne",
-			"county_code": "PS",
-			"latitude": -87.553677,
-			"longitude": -59.453459
-		},
-		"website": "http://pasquier.com",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Visa",
+		"number": "4024007120838510",
+		"expiration": "11/24",
+		"owner": "Evelyn Haag"
 	},
 	{
-		"id": 9,
-		"firstname": "Charlotte",
-		"lastname": "Marion",
-		"email": "susan.caron@voila.fr",
-		"phone": "+4965916709063",
-		"birthday": "1999-12-29",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "85, rue Germain",
-			"streetName": "chemin de Pineau",
-			"buildingNumber": "47",
-			"city": "BoucherVille",
-			"zipcode": "71639",
-			"country": "Hong Kong",
-			"county_code": "MP",
-			"latitude": -88.043443,
-			"longitude": -151.418436
-		},
-		"website": "http://vidal.fr",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "Visa",
+		"number": "4539663111220023",
+		"expiration": "04/26",
+		"owner": "Danika Abbott"
 	},
 	{
-		"id": 10,
-		"firstname": "Paulette",
-		"lastname": "Gomes",
-		"email": "eric.paul@fernandez.fr",
-		"phone": "+2260706501365",
-		"birthday": "2002-03-28",
-		"gender": "female",
-		"address": {
-			"id": 0,
-			"street": "58, impasse de Robin",
-			"streetName": "rue Beno\u00eet Allard",
-			"buildingNumber": "552",
-			"city": "Torresdan",
-			"zipcode": "77385",
-			"country": "S\u00e9n\u00e9gal",
-			"county_code": "EG",
-			"latitude": 12.519065,
-			"longitude": -20.049705
-		},
-		"website": "http://bruneau.fr",
-		"image": "http://placeimg.com/640/480/people"
+		"type": "MasterCard",
+		"number": "4539556814256320",
+		"expiration": "04/26",
+		"owner": "Brennan Murazik"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4024007135604477",
+		"expiration": "09/26",
+		"owner": "Erich Mann"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4539003948076191",
+		"expiration": "11/24",
+		"owner": "Caden White"
+	},
+	{
+		"type": "Visa",
+		"number": "349690052202486",
+		"expiration": "08/25",
+		"owner": "Isaac Wunsch"
+	},
+	{
+		"type": "Visa",
+		"number": "4532518359144623",
+		"expiration": "07/25",
+		"owner": "Julio Koepp"
+	},
+	{
+		"type": "Discover Card",
+		"number": "377988810243841",
+		"expiration": "02/25",
+		"owner": "Albin Hansen"
+	},
+	{
+		"type": "MasterCard",
+		"number": "5460743634710245",
+		"expiration": "04/26",
+		"owner": "Zane Stark"
+	},
+	{
+		"type": "Visa",
+		"number": "2300501839933926",
+		"expiration": "05/24",
+		"owner": "Fay Ryan"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4716027439006",
+		"expiration": "10/26",
+		"owner": "Jarred Blanda"
+	},
+	{
+		"type": "Visa",
+		"number": "4539790805223",
+		"expiration": "11/24",
+		"owner": "Eugenia White"
+	},
+	{
+		"type": "Discover Card",
+		"number": "6011973455391849",
+		"expiration": "04/25",
+		"owner": "Gay Schmeler"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "2303254868198600",
+		"expiration": "05/26",
+		"owner": "Salvador Effertz"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4024007129219332",
+		"expiration": "03/27",
+		"owner": "Celestine Mosciski"
+	},
+	{
+		"type": "Visa",
+		"number": "2221031080406741",
+		"expiration": "07/24",
+		"owner": "Madge Rohan"
+	},
+	{
+		"type": "American Express",
+		"number": "5512989654927072",
+		"expiration": "08/24",
+		"owner": "Breanna Raynor"
+	},
+	{
+		"type": "MasterCard",
+		"number": "6011626148707207",
+		"expiration": "02/25",
+		"owner": "Humberto Rath"
+	},
+	{
+		"type": "American Express",
+		"number": "6011367636680149",
+		"expiration": "04/27",
+		"owner": "Ludwig Prosacco"
+	},
+	{
+		"type": "Visa",
+		"number": "4577679302986142",
+		"expiration": "12/24",
+		"owner": "Shayne Jacobson"
+	},
+	{
+		"type": "Visa",
+		"number": "6011468385387256",
+		"expiration": "04/25",
+		"owner": "Norbert Okuneva"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4532452961540798",
+		"expiration": "05/26",
+		"owner": "Orie Barton"
+	},
+	{
+		"type": "Visa",
+		"number": "4485811360948372",
+		"expiration": "09/25",
+		"owner": "Viva Sawayn"
+	},
+	{
+		"type": "MasterCard",
+		"number": "5123418345276104",
+		"expiration": "11/25",
+		"owner": "Mark Gutmann"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4497605803646",
+		"expiration": "03/26",
+		"owner": "Jazmyne Crooks"
+	},
+	{
+		"type": "MasterCard",
+		"number": "6011016191448083",
+		"expiration": "07/24",
+		"owner": "Jovan Terry"
+	},
+	{
+		"type": "Visa",
+		"number": "5414887145268735",
+		"expiration": "07/25",
+		"owner": "Grace Nienow"
+	},
+	{
+		"type": "American Express",
+		"number": "5174880506977085",
+		"expiration": "04/26",
+		"owner": "Amara Green"
+	},
+	{
+		"type": "Visa",
+		"number": "4716246583121",
+		"expiration": "12/24",
+		"owner": "Felicity Cummerata"
+	},
+	{
+		"type": "MasterCard",
+		"number": "2438287332228460",
+		"expiration": "06/24",
+		"owner": "Alison Romaguera"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4539571614530337",
+		"expiration": "08/26",
+		"owner": "Jaylon Franecki"
+	},
+	{
+		"type": "Visa",
+		"number": "6011774481250813",
+		"expiration": "06/26",
+		"owner": "Neoma Roberts"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4929753793059258",
+		"expiration": "10/24",
+		"owner": "Rubye Emard"
+	},
+	{
+		"type": "Visa",
+		"number": "6011158117344796",
+		"expiration": "09/24",
+		"owner": "Jerrold Hirthe"
+	},
+	{
+		"type": "American Express",
+		"number": "2659664853334699",
+		"expiration": "07/24",
+		"owner": "Caden Buckridge"
+	},
+	{
+		"type": "Visa",
+		"number": "4408577529586874",
+		"expiration": "12/24",
+		"owner": "Ibrahim Walter"
+	},
+	{
+		"type": "American Express",
+		"number": "2221086020008528",
+		"expiration": "12/25",
+		"owner": "Kody Roberts"
+	},
+	{
+		"type": "Visa",
+		"number": "5268770113935567",
+		"expiration": "01/27",
+		"owner": "Royal Dibbert"
+	},
+	{
+		"type": "Visa",
+		"number": "347213265989781",
+		"expiration": "02/26",
+		"owner": "Magdalen Hyatt"
+	},
+	{
+		"type": "Discover Card",
+		"number": "2430020354803714",
+		"expiration": "01/25",
+		"owner": "Camryn Abernathy"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4024007185278271",
+		"expiration": "12/24",
+		"owner": "Justice Hodkiewicz"
+	},
+	{
+		"type": "MasterCard",
+		"number": "2348172003937095",
+		"expiration": "01/26",
+		"owner": "Theodora Fisher"
+	},
+	{
+		"type": "Visa",
+		"number": "343400967610021",
+		"expiration": "02/25",
+		"owner": "Kobe Sanford"
+	},
+	{
+		"type": "American Express",
+		"number": "5550014770276297",
+		"expiration": "01/25",
+		"owner": "Shemar Muller"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4829027244413690",
+		"expiration": "07/24",
+		"owner": "Dangelo West"
+	},
+	{
+		"type": "Visa",
+		"number": "4929564818694",
+		"expiration": "07/26",
+		"owner": "Jacey Rau"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4045204648314125",
+		"expiration": "05/24",
+		"owner": "Lavonne Schimmel"
+	},
+	{
+		"type": "Discover Card",
+		"number": "4539396520235700",
+		"expiration": "11/25",
+		"owner": "Maxie Hoeger"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4929303009787",
+		"expiration": "03/26",
+		"owner": "Andre Harris"
+	},
+	{
+		"type": "Visa",
+		"number": "2221678525322527",
+		"expiration": "10/25",
+		"owner": "Haylee Barrows"
+	},
+	{
+		"type": "Visa",
+		"number": "4556302097298624",
+		"expiration": "07/24",
+		"owner": "Herta Bartell"
+	},
+	{
+		"type": "MasterCard",
+		"number": "2489140909442485",
+		"expiration": "04/25",
+		"owner": "Andres Donnelly"
+	},
+	{
+		"type": "Visa",
+		"number": "5141976960579928",
+		"expiration": "02/26",
+		"owner": "Vernon Funk"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4532319205290904",
+		"expiration": "01/27",
+		"owner": "Alberto Steuber"
+	},
+	{
+		"type": "American Express",
+		"number": "4444175504443918",
+		"expiration": "12/25",
+		"owner": "Cleta Ziemann"
+	},
+	{
+		"type": "American Express",
+		"number": "4485065759634743",
+		"expiration": "12/26",
+		"owner": "Schuyler Rath"
+	},
+	{
+		"type": "Visa",
+		"number": "5567778101804107",
+		"expiration": "11/26",
+		"owner": "Lisette Schuster"
+	},
+	{
+		"type": "MasterCard",
+		"number": "2348245149658302",
+		"expiration": "02/26",
+		"owner": "Lori Veum"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4539978230810904",
+		"expiration": "11/26",
+		"owner": "Pearl Johnston"
+	},
+	{
+		"type": "Visa",
+		"number": "6011845541464667",
+		"expiration": "02/26",
+		"owner": "Sonny Weissnat"
+	},
+	{
+		"type": "Visa",
+		"number": "6011318057616963",
+		"expiration": "03/26",
+		"owner": "Miguel Wehner"
+	},
+	{
+		"type": "Visa",
+		"number": "5467436531296748",
+		"expiration": "10/24",
+		"owner": "Luther Trantow"
+	},
+	{
+		"type": "MasterCard",
+		"number": "5306374666623227",
+		"expiration": "07/26",
+		"owner": "Jerrell Hartmann"
+	},
+	{
+		"type": "Visa",
+		"number": "5454363670098039",
+		"expiration": "10/25",
+		"owner": "Buster Abshire"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4011615892990395",
+		"expiration": "06/26",
+		"owner": "Peyton Kohler"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4532735529140386",
+		"expiration": "06/26",
+		"owner": "Marques Lind"
+	},
+	{
+		"type": "American Express",
+		"number": "4556171554014364",
+		"expiration": "12/25",
+		"owner": "Greg Howell"
+	},
+	{
+		"type": "Visa",
+		"number": "4916362343231",
+		"expiration": "03/25",
+		"owner": "Rachelle Schamberger"
+	},
+	{
+		"type": "Visa",
+		"number": "2659187622642861",
+		"expiration": "06/25",
+		"owner": "Lyric Jakubowski"
+	},
+	{
+		"type": "American Express",
+		"number": "4024007199823310",
+		"expiration": "05/24",
+		"owner": "Daren Botsford"
+	},
+	{
+		"type": "Visa",
+		"number": "2666106292920861",
+		"expiration": "10/25",
+		"owner": "Lilyan Streich"
+	},
+	{
+		"type": "MasterCard",
+		"number": "5186032806253694",
+		"expiration": "12/25",
+		"owner": "Roberto Torp"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4024007176913050",
+		"expiration": "03/25",
+		"owner": "Michele Goodwin"
+	},
+	{
+		"type": "MasterCard",
+		"number": "2395781080842788",
+		"expiration": "05/25",
+		"owner": "Jade McGlynn"
+	},
+	{
+		"type": "Visa",
+		"number": "4916604559561646",
+		"expiration": "02/25",
+		"owner": "Israel McClure"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4929398689448544",
+		"expiration": "05/26",
+		"owner": "Nils Bosco"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4485732989994375",
+		"expiration": "01/26",
+		"owner": "Marielle Collier"
+	},
+	{
+		"type": "MasterCard",
+		"number": "348971986588476",
+		"expiration": "05/26",
+		"owner": "Kristina Hoppe"
+	},
+	{
+		"type": "Visa",
+		"number": "4556916514432316",
+		"expiration": "03/25",
+		"owner": "Rafael Ebert"
+	},
+	{
+		"type": "Visa",
+		"number": "5504126197283644",
+		"expiration": "11/25",
+		"owner": "Arjun Stanton"
+	},
+	{
+		"type": "Visa",
+		"number": "2720360756054352",
+		"expiration": "01/25",
+		"owner": "David Maggio"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "4929405526797874",
+		"expiration": "02/26",
+		"owner": "Ricardo Cummerata"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "5457065102277050",
+		"expiration": "03/25",
+		"owner": "Savion Grimes"
+	},
+	{
+		"type": "Visa",
+		"number": "4532498905015876",
+		"expiration": "07/26",
+		"owner": "Zola Rutherford"
+	},
+	{
+		"type": "Visa",
+		"number": "4929169707572635",
+		"expiration": "09/25",
+		"owner": "Albin Nitzsche"
+	},
+	{
+		"type": "MasterCard",
+		"number": "4929905172089997",
+		"expiration": "08/25",
+		"owner": "Joe Rohan"
+	},
+	{
+		"type": "American Express",
+		"number": "4556445850217750",
+		"expiration": "02/27",
+		"owner": "Cecelia Legros"
+	},
+	{
+		"type": "Visa",
+		"number": "2468378027856898",
+		"expiration": "02/27",
+		"owner": "Ashleigh Little"
+	},
+	{
+		"type": "American Express",
+		"number": "2610252291302276",
+		"expiration": "09/25",
+		"owner": "Kody Ferry"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "2221456401432771",
+		"expiration": "02/25",
+		"owner": "Laurence Bruen"
+	},
+	{
+		"type": "Visa Retired",
+		"number": "6011335666270271",
+		"expiration": "06/26",
+		"owner": "Amelia Smith"
+	},
+	{
+		"type": "Discover Card",
+		"number": "4532594672920478",
+		"expiration": "05/26",
+		"owner": "Elwin Klein"
+	},
+	{
+		"type": "Visa",
+		"number": "2534545768794971",
+		"expiration": "02/27",
+		"owner": "Marques Sauer"
 	}
 ]
 
-/*Agrupar a las personas que viven en una direccion con un 8 en el codigo postal */
-/*
-let people = []
-data.forEach((personaInfo) => {
-	let zip = personaInfo["address"]["zipcode"]
-	if (zip.includes("3"))
-		people.push(personaInfo)
-}) */
+let cardTypes = [];
 
-let people = data.filter((personaInfo) => personaInfo["address"]["zipcode"].includes("9"))
-
-const root = document.getElementById("root")
-console.log(root)
-people.forEach((personaInfo) => {
-	const nameParraf = document.createElement("p")
-	nameParraf.classList.add("nameText")
-	nameParraf.innerText = `Nombre: ${personaInfo.firstname} ${personaInfo.lastname}`
-	root.appendChild(nameParraf)
+cards.forEach((cardSingular) => {
+    if (!cardTypes.includes(cardSingular.type))
+        cardTypes.push(cardSingular.type);
 })
 
- 
 
+let cardsGrouped = {}
+
+cardTypes.forEach((type) => {
+
+	cardsGrouped[type] = cards.filter((card) => card.type === type)
+})
+console.log(cardsGrouped)
+const value = prompt()
+console.log(cardsGrouped[value])
