be4b54389a7833859887c6ab34f087f87a1e4d44
Héctor Romero López <hector@hromerol.es>
2024-04-16 17:24:04 +0200

    New ej

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ejercicio Usuarios
+
+1. Consumir (fetch) la API de usuarios https://fakerapi.it/api/v1/users?_quantity=20&_gender=male 
+2. Crear las clases de Persona y Usuarios, teniendo en cuenta que tienen propiedades comunes
+2.1 Mostrarlo en HTML los resultados de usuarios. DE LA FORMA MÁS SENCILLA POSIBLE
+3. Crear un input que permita filtrar por nombre de la persona
+4. Tener un input de número que permita especificar un número de valores que obtener de la API
+    EJ: Si quiero obtener 90 valores, poder poner 90 en un input HTML y que se haga la petición a la API.
+    La url seria https://fakerapi.it/api/v1/users?_quantity=90&_gender=male 
+
+PD: El input de buscar por nombre y el de seleccionar el número de elementos para la llamada de la API. Estarán en HTML
--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+const Alejandro = new Persona("Alejandro", "Luis Acosta", 1);
+console.log(Alejandro);
+console.log(Alejandro.name);
 
-const Alejandro = new Persona("Alejandro","Luis Acosta", 1)
-console.log(Alejandro)
-console.log(Alejandro.name)
+try {
+	const Lucia = new Estudiante("Lucia", "Macho Sanchez", 2, "Mod 2", "34");
+	console.log(Lucia);
+} catch (error) {
+	console.log(error);
+}
 
-const Lucia = new Estudiante("Lucia", "Macho Sanchez",2,"Mod 2","Intermedio")
-console.log(Lucia)
+const request = fetch(
+	// Esto es un html entonces entra en PROBLEMAS CON LA INFO"https://api.ratick.hromerol.es/admin"
+    "https://fakerapi.it/api/v1/credit_cards?_quantity=100"
+)
+	.then((response) => {
+		if (response.ok) {
+			response
+				.json()
+				.then((jsonData) => {})
+				.catch((error) => alert("Problemas con la info de la API"));
+		}
+	})
+	.catch((error) => alert("Error en la api"));
 
